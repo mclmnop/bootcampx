bccb5355a895212562e11be296d377c5c0f9c424
mclmnop <m_charlebois@hotmail.com>
2021-03-03 20:46:55 -0500

    changed user input for parametrized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -59,7 +59,7 @@ LIMIT $2;
 })
 .catch(err => console.error('query error', err.stack)); */
 
-pool.query(`
+/* pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
@@ -70,4 +70,23 @@ LIMIT ${process.argv[3] || 5};
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-}).catch(err => console.error('query error', err.stack));
+}).catch(err => console.error('query error', err.stack)); */
+
+const cohort_name = process.argv[2]
+const limit = process.argv[3]
+let inputArray = [cohort_name, limit]
+
+
+pool.query(`
+SELECT cohorts.name as cohort, students.name as name, students.id as id 
+FROM students
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = $1
+LIMIT $2;
+`, inputArray)  
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`)
+  })
+})
+.catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-console.log(process.argv[2])
+const cohort_name = [process.argv[2]]
 
 pool.query(`
 SELECT  teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances 
@@ -14,10 +14,10 @@ FROM teachers
 JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
 JOIN students ON assistance_requests.student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = ${process.argv[2].toString()}
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teacher;
-`)
+`, cohort_name)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
